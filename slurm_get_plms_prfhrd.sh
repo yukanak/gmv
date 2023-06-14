20afad045147ff338833c561dfca13e1f5493c32
Yuka Nakato <nakat013@umn.edu>
2023-06-14 15:50:35 -0700

    Submitting jobs

---
+++ slurm_get_plms_prfhrd.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --job-name=get_plms_prfhrd
+#SBATCH --time=14:00:00
+#SBATCH --array=1-9
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=128G
+#SBATCH --partition=kipac
+
+ests=(TT TE EE TB EB TTprf all TTEETE TBEB)
+qe=${ests[$SLURM_ARRAY_TASK_ID-1]}
+
+~/gmv/get_plms_prfhrd.py $qe 100
