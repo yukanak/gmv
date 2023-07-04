9d7b307fc4f2e3f8a00b9cf49ddd0087eff34f87
Yuka Nakato <nakat013@umn.edu>
2023-07-04 12:16:40 -0700

    Whoops

--- slurm_get_plms_prfhrd.sh
+++ slurm_get_plms_prfhrd.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #SBATCH --job-name=get_plms_prfhrd
 #SBATCH --time=14:00:00
-#SBATCH --array=1-9
+#SBATCH --array=1-90
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=128G
 #SBATCH --partition=kipac
 
 ests=(TT TE EE TB EB TTprf all TTEETE TBEB)
-qe=${ests[$SLURM_ARRAY_TASK_ID-1]}
-sim1=$(((SLURM_ARRAY_TASK_ID-1)+100))
-sim2=$(((SLURM_ARRAY_TASK_ID-1)+100+1))
+qe=${ests[$SLURM_ARRAY_TASK_ID%9-1]}
+sim1=$(((SLURM_ARRAY_TASK_ID-1)/9+100))
+sim2=$(((SLURM_ARRAY_TASK_ID-1)/9+100+1))
 
 ~/gmv/get_plms_prfhrd.py $qe $sim1 $sim2
