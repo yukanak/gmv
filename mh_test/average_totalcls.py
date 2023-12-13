d5901dcd4cdf3f4db63dd1db839ad64cf4b116bd
Yuka Nakato <nakat013@umn.edu>
2023-12-13 10:57:38 -0800

    Random

--- mh_test/average_totalcls.py
+++ mh_test/average_totalcls.py
@@ -18,34 +18,25 @@ l = np.arange(0,lmax+1)
 ell,sltt,slee,slbb,slte = utils.get_lensedcls('/home/users/yukanaka/healqest/healqest/camb/planck2018_base_plikHM_TTTEEE_lowl_lowE_lensing_lensedCls.dat' ,lmax=lmax)
 n = 40
 
-cltt = np.zeros(lmax+1)
+cltt1 = np.zeros(lmax+1)
+cltt2 = np.zeros(lmax+1)
 clee = np.zeros(lmax+1)
 clbb = np.zeros(lmax+1)
 clte = np.zeros(lmax+1)
-cltt_mv = np.zeros(lmax+1)
-cltt_tszn = np.zeros(lmax+1)
 for i in np.arange(n)+1:
     totalcls = np.load(dir_out+f'totalcls/totalcls_seed1_{i}_seed2_{i}_lmaxT{lmaxT}_lmaxP{lmaxP}_nside{nside}_{append}.npy')
-    totalcls_mv = np.load(dir_out+f'totalcls/totalcls_mvTT_seed1_{i}_seed2_{i}_lmaxT{lmaxT}_lmaxP{lmaxP}_nside{nside}_{append}.npy')
-    totalcls_tszn = np.load(dir_out+f'totalcls/totalcls_tsznulledTT_seed1_{i}_seed2_{i}_lmaxT{lmaxT}_lmaxP{lmaxP}_nside{nside}_{append}.npy')
-    cltt += totalcls[:,0]
-    clee += totalcls[:,1]
-    clbb += totalcls[:,2]
-    clte += totalcls[:,3]
-    cltt_mv += totalcls_mv[:,0]
-    cltt_tszn += totalcls_tszn[:,0]
-cltt /= 40
+    cltt1 += totalcls[:,0]
+    cltt2 += totalcls[:,1]
+    clee += totalcls[:,2]
+    clbb += totalcls[:,3]
+    clte += totalcls[:,4]
+cltt1 /= 40
+cltt2 /= 40
 clee /= 40
 clbb /= 40
 clte /= 40
-cltt_mv /= 40
-cltt_tszn /= 40
-totalcls_avg = np.vstack((cltt,clee,clbb,clte)).T
-totalcls_avg_mv = np.vstack((cltt_mv,clee,clbb,clte)).T
-totalcls_avg_tszn = np.vstack((cltt_tszn,clee,clbb,clte)).T
+totalcls_avg = np.vstack((cltt1,cltt2,clee,clbb,clte)).T
 np.save(dir_out+f'totalcls/totalcls_average_lmaxT{lmaxT}_lmaxP{lmaxP}_nside{nside}_{append}.npy',totalcls_avg)
-np.save(dir_out+f'totalcls/totalcls_mvTT_average_lmaxT{lmaxT}_lmaxP{lmaxP}_nside{nside}_{append}.npy',totalcls_avg_mv)
-np.save(dir_out+f'totalcls/totalcls_tsznulledTT_average_lmaxT{lmaxT}_lmaxP{lmaxP}_nside{nside}_{append}.npy',totalcls_avg_tszn)
 
 plt.figure(0)
 plt.clf()
@@ -53,10 +44,11 @@ plt.plot(ell, sltt, color='firebrick', linestyle='-', label='sltt')
 plt.plot(ell, slee, color='forestgreen', linestyle='-', label='slee')
 plt.plot(ell, slbb, color='darkblue', linestyle='-', label='slbb')
 plt.plot(ell, slte, color='gold', linestyle='-', label='slte')
-plt.plot(ell, totalcls_avg[:,0], color='pink', linestyle='--', label='total TT')
-plt.plot(ell, totalcls_avg[:,1], color='lightgreen', linestyle='--', label='total EE')
-plt.plot(ell, totalcls_avg[:,2], color='powderblue', linestyle='--', label='total BB')
-plt.plot(ell, totalcls_avg[:,3], color='palegoldenrod', linestyle='--', label='total TE')
+plt.plot(ell, totalcls_avg[:,0], color='pink', linestyle='--', label='total TT1')
+plt.plot(ell, totalcls_avg[:,1], color='darkorchid', linestyle='--', label='total TT2')
+plt.plot(ell, totalcls_avg[:,2], color='lightgreen', linestyle='--', label='total EE')
+plt.plot(ell, totalcls_avg[:,3], color='powderblue', linestyle='--', label='total BB')
+plt.plot(ell, totalcls_avg[:,4], color='palegoldenrod', linestyle='--', label='total TE')
 plt.xscale('log')
 plt.yscale('log')
 plt.xlim(10,lmax)
--- mh_test/slurm_get_plms_unified.sh
+++ mh_test/slurm_get_plms_unified.sh
@@ -13,5 +13,5 @@ sim2=$(((SLURM_ARRAY_TASK_ID-1)/11+2))
 
 export OMP_NUM_THREADS=12
 
-~/gmv/mh_test/get_plms_unified.py $qe $sim1 $sim2
+~/gmv/mh_test/get_plms_unified.py $qe $sim1 $sim1
 #~/gmv/mh_test/get_plms_unified.py TT $SLURM_ARRAY_TASK_ID $SLURM_ARRAY_TASK_ID
