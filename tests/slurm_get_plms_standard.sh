4cd0c056bdd42365d0d6e8eef6bfc22b805111f0
Yuka Nakato <nakat013@umn.edu>
2024-03-18 16:48:10 -0700

    Remaking the standard case with foregrounds included in input sims for apples-to-apples comparison in reconstruction noise with cross-ILC and MH results

---
+++ tests/slurm_get_plms_standard.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --job-name=get_plms_standard
+#SBATCH --time=1:00:00
+#SBATCH --array=1-100
+#SBATCH --cpus-per-task=12
+#SBATCH --mem=100G
+#SBATCH --partition=kipac
+
+ests=(TT TE ET EE TB BT EB BE all TTEETE TBEB)
+qe=${ests[$SLURM_ARRAY_TASK_ID%11-1]}
+sim1=$(((SLURM_ARRAY_TASK_ID-1)/11+1))
+sim2=$(((SLURM_ARRAY_TASK_ID-1)/11+2))
+
+export OMP_NUM_THREADS=12
+
+~/gmv/tests/get_plms_standard.py TT $SLURM_ARRAY_TASK_ID $SLURM_ARRAY_TASK_ID standard
+
+#~/gmv/tests/get_plms_standard.py $qe $sim1 $sim1 standard
+#~/gmv/tests/get_plms_standard.py $qe $sim1 $sim2 standard
+#~/gmv/tests/get_plms_standard.py $qe $sim2 $sim1 standard
+#~/gmv/tests/get_plms_standard.py $qe $sim1 $sim1 standard_cmbonly_phi1_tqu1tqu2
+#~/gmv/tests/get_plms_standard.py $qe $sim1 $sim1 standard_cmbonly_phi1_tqu2tqu1
+#~/gmv/tests/get_plms_standard.py $qe $sim1 $sim2 standard_cmbonly
+#~/gmv/tests/get_plms_standard.py $qe $sim2 $sim1 standard_cmbonly
